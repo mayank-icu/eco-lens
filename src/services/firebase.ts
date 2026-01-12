6b653df0f38c2b2494068b42d4ffd8231151fb30
Mayank Kumar <themayankgamerz@gmail.com>
2026-01-12 22:16:53 +0530

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,13 +7,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyAhM0JcNuF27zEzWKqV1KtQuS9mrH-lG2s",
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "plasti-sort.firebaseapp.com",
-    projectId: process.env.FIREBASE_PROJECT_ID || "plasti-sort",
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "plasti-sort.firebasestorage.app",
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "493205509472",
-    appId: process.env.FIREBASE_APP_ID || "1:493205509472:web:95748e6a5a79a5087d7a05",
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-C27K1XM49S"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN ,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
